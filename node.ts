1b244541b42aefbbe6cacc428c1f00836b826093
tenderjin <112069806+tenderjin@users.noreply.github.com>
2025-02-14 13:35:15 +0800

    Add files via upload

---
+++ node.ts
@@ -0,0 +1,11 @@
+import { app } from './src/app.ts'
+import { config } from './src/config.ts'
+
+globalThis.env = process.env as Record<string, string>
+
+console.log(`service is running at http://localhost:${config.port}`)
+
+await app.listen({
+  hostname: config.host,
+  port: config.port,
+})
